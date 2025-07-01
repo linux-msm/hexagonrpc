a7f20fabe8c72cfa41fb3be6dfe11562508dec5c
Richard Acayan <mailingradian@gmail.com>
2025-07-01 17:14:37 -0400

    hexagonrpcd: use hexagonrpc handle API for default listener
    The handle API is a high-level interface that puts all error messages in
    the same buffer. Use the handle API to open and close the
    adsp_default_listener remote interface.
    
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>

--- hexagonrpcd/rpcd.c
+++ hexagonrpcd/rpcd.c
@@ -23,7 +23,9 @@
 #include <fcntl.h>
 #include <libhexagonrpc/error.h>
 #include <libhexagonrpc/fastrpc.h>
+#include <libhexagonrpc/handle.h>
 #include <libhexagonrpc/interfaces/remotectl.def>
+#include <libhexagonrpc/session.h>
 #include <misc/fastrpc.h>
 #include <unistd.h>
 #include <signal.h>
@@ -43,87 +45,31 @@
 #include "localctl.h"
 #include "rpcd_builder.h"
 
-static int remotectl_open(int fd, char *name, struct fastrpc_context **ctx, void (*err_cb)(const char *err))
+static int adsp_default_listener_register(int fd, uint32_t handle)
 {
-	uint32_t handle;
-	int32_t dlret;
-	char err[256];
-	int ret;
-
-	ret = fastrpc2(&remotectl_open_def, fd, REMOTECTL_HANDLE,
-		       strlen(name) + 1, name,
-		       &handle,
-		       &dlret,
-		       256, err);
-
-	if (ret) {
-		err_cb(hexagonrpc_strerror(errno));
-		return ret;
-	}
-
-	if (dlret) {
-		err_cb(err);
-		return dlret;
-	}
-
-	*ctx = fastrpc_create_context(fd, handle);
-
-	return ret;
-}
-
-static int remotectl_close(struct fastrpc_context *ctx, void (*err_cb)(const char *err))
-{
-	uint32_t dlret;
-	char err[256];
-	int ret;
-
-	ret = fastrpc2(&remotectl_close_def, ctx->fd, REMOTECTL_HANDLE,
-		       ctx->handle,
-		       &dlret,
-		       256, err);
-
-	if (ret) {
-		err_cb(hexagonrpc_strerror(errno));
-		return ret;
-	}
-
-	if (dlret) {
-		err_cb(err);
-		return dlret;
-	}
-
-	fastrpc_destroy_context(ctx);
-
-	return ret;
-}
-
-static int adsp_default_listener_register(struct fastrpc_context *ctx)
-{
-	return fastrpc(&adsp_default_listener_register_def, ctx);
-}
-
-static void remotectl_err(const char *err)
-{
-	fprintf(stderr, "Could not remotectl: %s\n", err);
+	return fastrpc2(&adsp_default_listener_register_def, fd, handle);
 }
 
 static int register_fastrpc_listener(int fd)
 {
-	struct fastrpc_context *ctx;
+	uint32_t hdl;
+	char err[256];
 	int ret;
 
-	ret = remotectl_open(fd, "adsp_default_listener", &ctx, remotectl_err);
-	if (ret)
+	ret = hexagonrpc_open(fd, "adsp_default_listener", &hdl, 256, err);
+	if (ret) {
+		fprintf(stderr, "Could not open remote interface: %s\n", err);
 		return 1;
+	}
 
-	ret = adsp_default_listener_register(ctx);
+	ret = adsp_default_listener_register(fd, hdl);
 	if (ret) {
 		fprintf(stderr, "Could not register ADSP default listener\n");
 		goto err;
 	}
 
 err:
-	remotectl_close(ctx, remotectl_err);
+	hexagonrpc_close(fd, hdl);
 	return ret;
 }
 
