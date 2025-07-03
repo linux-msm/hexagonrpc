3b0396820e2fee9dbef2632813f60bb6e85fbd45
Richard Acayan <mailingradian@gmail.com>
2025-07-03 18:32:38 -0400

    hexagonrpcd: listener: remove interp2 support
    With the remote method definitions converted to interp4, the interp2
    definitions are no longer needed. Replace the interp2 definitions with
    the interp4 definitions recently added.
    
    Signed-off-by: Richard Acayan <mailingradian@gmail.com>

--- hexagonrpcd/listener.h
+++ hexagonrpcd/listener.h
@@ -22,7 +22,6 @@
 #ifndef LISTENER_H
 #define LISTENER_H
 
-#include <libhexagonrpc/fastrpc.h>
 #include <libhexagonrpc/hexagonrpc.h>
 #include <stddef.h>
 #include <stdint.h>
@@ -30,8 +29,7 @@
 #include "iobuffer.h"
 
 struct fastrpc_function_impl {
-	const struct fastrpc_function_def_interp2 *def;
-	const struct hrpc_method_def_interp4 *def4;
+	const struct hrpc_method_def_interp4 *def;
 	uint32_t (*impl)(void *data,
 			 const struct fastrpc_io_buffer *inbufs,
 			 struct fastrpc_io_buffer *outbufs);
